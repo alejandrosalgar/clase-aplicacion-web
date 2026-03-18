d9196a22d3db99a0bb137eeb8c79c956a32d38ae
dayanapiedrahita <dayanapiedrahita98@gmail.com>
2026-03-17 21:38:55 -0500

    feat: yml para pull y push

--- src/entities/usuarios.py
+++ src/entities/usuarios.py
@@ -1,8 +1,7 @@
 import uuid
 
-from sqlalchemy import Boolean, Column, DateTime, String, ForeignKey
+from sqlalchemy import Boolean, Column, DateTime, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from src.database.config import Base
